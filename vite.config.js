29597ab4c9b1c052815679d755f2197703ef0a91
programadorFB <devfuzabalta@gmail.com>
2025-11-10 16:55:36 -0300

    versão paga

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Redireciona chamadas de login para o server.js
+      // Garanta que o target seja este:
       '/login': {
         target: 'https://roleta-fuza.sortehub.online',
         changeOrigin: true,
       },
-      // Redireciona chamadas do jogo para o server.js
       '/start-game': {
         target: 'https://roleta-fuza.sortehub.online',
         changeOrigin: true,
       },
-      // Redireciona chamadas da API (histórico) para o server.js
       '/api': {
         target: 'https://roleta-fuza.sortehub.online',
         changeOrigin: true,
