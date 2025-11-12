6a09708ddf0b4558a41ea64e3f090c331276f4d3
programadorFB <devfuzabalta@gmail.com>
2025-11-12 19:06:40 -0300

    vai

--- src/utils/csvService.js
+++ src/utils/csvService.js
@@ -11,7 +11,14 @@ const CSV_HEADER = 'timestamp,signalId,gameId,signal\n';
 
 // Nomes das fontes de dados
 // ADICIONADAS AS NOVAS FONTES: speed, xxxtreme, vipauto
-export const SOURCES = ['immersive', 'brasileira', 'default', 'speed', 'xxxtreme', 'vipauto'];
+export const SOURCES = [
+    'immersive', 'brasileira', 'default', 'speed', 'xxxtreme', 'vipauto',
+    'auto', 'macau', 'footballstudio', 'vip', 'lightning', 'rapida2', 
+    'blazemega', 'aovivo', 'speedauto', 'lucky6', 'azure', 'romena', 
+    'hippodrome', 'verde', 'ruby', 'viproulette', 'relampago', 'russa', 
+    'turca', 'espanhola', 'malta', 'mega', 'italiana', 'powerup', 
+    'roulette', 'grandcasino', 'alema', 'rapida1', 'salonprive'
+];
 
 // --- Estado em Memória ---
 // Agora armazena um Set de IDs para cada fonte
